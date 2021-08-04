343d517f38c34e2fbd1e2d3ab77353e763a7dc3f
Steven Linde <stevenlinde@google.com>
2021-08-04 13:39:11 -0700

    moved the google_gke_hub_feature into its own file

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,17 +56,6 @@ resource "google_gke_hub_membership" "membership" {
   ]
 }
 
-resource "google_gke_hub_feature" "acm" {
-  name     = "configmanagement"
-  location = "global"
-
-  provider = google-beta
-  depends_on = [
-    google_project_service.hub,
-    google_project_service.acm
-  ]
-}
-
 resource "google_gke_hub_feature_membership" "feature_member" {
   provider   = google-beta
   location   = "global"
