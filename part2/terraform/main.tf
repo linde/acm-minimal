b654f00097403c88fc9e1b6c56b3f78dcce452e6
Steven Linde <stevenlinde@google.com>
2021-08-09 00:49:57 +0000

    adding referential, it was needed for the bundle

--- part2/terraform/main.tf
+++ part2/terraform/main.tf
@@ -53,6 +53,7 @@ resource "google_gke_hub_feature_membership" "feature_member" {
     policy_controller {
       enabled                    = true
       template_library_installed = true
+      referential_rules_enabled  = true
     }
   }
 }
