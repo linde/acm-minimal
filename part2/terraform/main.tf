c96b2cf47079e900b64b319cb050781aafc9cf51
Steven Linde <stevenlinde@google.com>
2021-08-12 02:45:49 +0000

    pined the version of config management

--- part2/terraform/main.tf
+++ part2/terraform/main.tf
@@ -40,6 +40,7 @@ resource "google_gke_hub_feature_membership" "feature_member" {
   feature    = "configmanagement"
   membership = google_gke_hub_membership.membership.membership_id
   configmanagement {
+    version = "1.8.1"
     config_sync {
       source_format = "unstructured"
       git {
